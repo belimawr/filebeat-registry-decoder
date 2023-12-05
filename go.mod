1eb7033e1ce659b7cdbefc9f8b416f869eef23b0
Tiago Queiroz <tiago.queiroz@elastic.co>
2023-12-05 09:57:48 +0100

    Decode any Filebeat registry file

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
-module github.com/belimawr/registry-decoder
+module github.com/belimawr/filebeat-registry-decoder
 
-go 1.18
+go 1.20
 
 require github.com/elastic/beats/v7 v7.17.9
 
--- main.go
+++ main.go
@@ -7,68 +7,23 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/elastic/beats/v7/libbeat/common/transform/typeconv"
 )
-
-func main() {
-	file, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatalf("cannot read file %q, error: %s", os.Args[1], err)
-	}
-	defer file.Close()
-
-	// out, err := os.Create(os.Args[2])
-	// if err != nil {
-	// 	log.Fatalf("cannot create output file %q, error: %s", os.Args[2], err)
-	// }
-	// defer out.Close()
-
-	processData(file, os.Stdout)
-}
-
-func processData(in io.Reader, out io.Writer) error {
-	s := bufio.NewScanner(in)
-	var line uint64
-	for s.Scan() {
-		// First line is always an "operation"
-		// so just print it again
-		line++
-		opLine := s.Bytes()
-		out.Write(opLine)
-
-		out.Write([]byte("\n"))
-		// Second line is always an entry
-		// decode and "pretty print" it
-
-		if !s.Scan() {
-			return nil
-		}
-
-		line++
-		entryLine := s.Bytes()
-		v, err := f(entryLine)
-		if err != nil {
-			panic(err)
-		}
-
-		d, err := json.Marshal(v)
-		if err != nil {
-			panic(err)
-		}
-
-		out.Write(d)
-
-		if _, err := out.Write([]byte("\n")); err != nil {
-			return fmt.Errorf("could not write line break after line %d, error: %s", line, err)
-		}
-	}
-
-	return nil
-}
 
 type regOp struct {
 	Op string `json:"op"`
 	ID int    `json:"id"`
+}
+
+type checkpointEntry struct {
+	Cursor  *Cursor       `json:"cursor"`
+	Key     string        `json:"_key"`
+	Meta    *Meta         `json:"meta"`
+	TTL     time.Duration `json:"ttl"`
+	Updated time.Time     `json:"updated"`
 }
 
 type regEntry struct {
@@ -91,4 +46,147 @@ type Value struct {
 	Meta    *Meta     `json:"meta"`
 	TTL     int64     `json:"ttl"`
 	Updated time.Time `json:"updated"`
+}
+
+type entry struct {
+	K     string                 `json:"k"`
+	Value map[string]interface{} `json:"v"`
+}
+
+func (e entry) Decode(to interface{}) error {
+	return typeconv.Convert(to, e.Value)
+}
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatalf("cannot read file %q, error: %s", os.Args[1], err)
+	}
+	defer file.Close()
+
+	if err := processData(file, os.Stdout); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func processLine(line string) (string, error) {
+	switch {
+	case strings.Contains(line, "\"_key\""):
+		l, err := decodeCheckpointEntry(line)
+		if err != nil {
+			return "", err
+		}
+		return l, nil
+	case strings.Contains(line, "\"k\""):
+		l, err := decodeLogEntry(line)
+		if err != nil {
+			return "", err
+		}
+		return l, nil
+	case strings.Contains(line, "\"op\""):
+		l, err := decodeRegOp(line)
+		if err != nil {
+			return "", err
+		}
+		return l, nil
+		// the checkpoint file is a JSON array, skip the square brackets
+	case line == "[" || line == "]":
+		return "", nil
+	default:
+		return "", fmt.Errorf("unknown format: %q", line)
+	}
+}
+
+func decodeCheckpointEntry(line string) (string, error) {
+	if strings.HasSuffix(line, ",") {
+		line = line[:len(line)-1]
+	}
+	m := map[string]any{}
+	if err := json.Unmarshal([]byte(line), &m); err != nil {
+		return "", fmt.Errorf("cannot decode line into map[string]any: %w", err)
+	}
+
+	e := checkpointEntry{}
+	if err := typeconv.Convert(&e, m); err != nil {
+		return "", fmt.Errorf("cannot use typeconv to decode line '%q', err: %w", line, err)
+	}
+
+	key, ok := m["_key"].(string)
+	if !ok {
+		return "", fmt.Errorf("'_key' is %T instead of string", m["_key"])
+	}
+	e.Key = key
+
+	finalBytes, err := json.Marshal(e)
+	if err != nil {
+		return "", fmt.Errorf("could not encode checkpoint entry as JSON, err: %w", err)
+	}
+	return string(finalBytes), nil
+}
+
+// decodeRegOp ensures line is in the correct format
+func decodeRegOp(line string) (string, error) {
+	op := regOp{}
+	if err := json.Unmarshal([]byte(line), &op); err != nil {
+		return "", fmt.Errorf("cannot decode line: %w", err)
+	}
+
+	if op.ID == 0 || op.Op == "" {
+		return "", fmt.Errorf("%q is not a valid registry operation", line)
+	}
+
+	return line, nil
+}
+
+func processData(in io.Reader, out io.Writer) error {
+	s := bufio.NewScanner(in)
+	var lineCount uint64
+	for s.Scan() {
+		opLine := s.Text()
+		line, err := processLine(opLine)
+		if err != nil {
+			return fmt.Errorf("could not process line %d, err: %w", lineCount, err)
+		}
+
+		// no need to write a blank line
+		if line == "" {
+			continue
+		}
+
+		if _, err := out.Write([]byte(line)); err != nil {
+			return fmt.Errorf("could not write line %d, err: %w", lineCount, err)
+		}
+		if _, err := out.Write([]byte("\n")); err != nil {
+			return fmt.Errorf("could not write line break after line %d, error: %s", lineCount, err)
+		}
+		lineCount++
+	}
+
+	return nil
+}
+
+func decodeLogEntry(line string) (string, error) {
+	var dec entry
+	var st Value
+
+	if err := json.Unmarshal([]byte(line), &dec); err != nil {
+		panic(err)
+	}
+
+	if err := dec.Decode(&st); err != nil {
+		log.Fatalf("Failed to read regisry state for '%q', error: %#v", dec.K, err)
+		return "", nil
+	}
+
+	ret := regEntry{
+		K: dec.K,
+		V: st,
+	}
+
+	humanRadable, err := json.Marshal(ret)
+	if err != nil {
+		return "", fmt.Errorf("could not encode registry entry as JSON: %w", err)
+	}
+
+	return string(humanRadable), nil
 }
